7cd186cc8aaccd117f483aea64cc4a0b02ffe0c5
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-09 17:29:52 -0500

    init

---
+++ asyncBreeds.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    if (!error) callback(data);
+    else callback(undefined);
+  });
+};
+
+module.exports = breedDetailsFromFile;
